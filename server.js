5ad1cdaf151698c949697111e2600c785299efe1
Ashrith Varghese <asrithalappat@gmail.com>
2025-06-20 18:46:56 +0530

    added cors

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import postRoutes from './routes/postRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 await connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req,res)=>{
     res.send("Welcome to blog api");
